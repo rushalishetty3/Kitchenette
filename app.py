e4eae1cd17f182c5920ad144b3d7e81f7315b7f9
Rushali <rushalishetty0@gmail.com>
2021-05-27 10:37:11 +0530

    comapring category

--- app.py
+++ app.py
@@ -22,13 +22,36 @@ app.layout = container.layout
 #interactive bar chart
 @app.callback(
     Output(component_id='bar_graph2', component_property='figure'),
-    [Input(component_id='bar_dropdown1', component_property='value'),Input(component_id='bar_dropdown2', component_property='value')]
+    [Input(component_id='bar_dropdown1', component_property='value'),Input(component_id='bar_dropdown2', component_property='value'),Input(component_id='bar_dropdown3', component_property='value')]
 )
-def update_barchart(selected_value1,selected_value2):
-    nutrients = ['carbohydrates_g', 'sugars_g', 'fat_g']
+def update_barchart(selected_value1,selected_value2,selected_value3):
+    nutrients = selected_value3
     category = [selected_value1, selected_value2]
 
-    bar_data = data.df.groupby("category").agg({'name':'count',nutrients[0]:'mean',nutrients[1]:'mean',nutrients[2]:'mean'}).reset_index()
+    daily_nutrients_intake = {
+        'carbohydrates_g' : 260,
+        'sugars_g' : 90,
+        'fat_g' : 70,
+        'protein_g' : 50,
+        'dietary_fiber_g' : 25,
+        'sodium_mg' : 2300,
+        'calcium_mg' : 1000,
+        'iron_mg' : 14.8,
+        'magnesium_mg' : 400,
+        'potassium_mg' : 4700,
+    }
+
+    daily_dose = []
+    for x in nutrients:
+        if(x.endswith('mg')):
+            daily_dose.append(daily_nutrients_intake[x]/1000)
+        else: 
+            daily_dose.append(daily_nutrients_intake[x])
+
+    dict_temp = dict(map((lambda x: (x,'mean')), nutrients))
+    dict_temp['name'] = 'count'
+
+    bar_data = data.df.groupby("category").agg(dict_temp).reset_index()
     bar_data = bar_data[bar_data['name']>100]
 
     category_nutrients = []
@@ -37,13 +60,17 @@ def update_barchart(selected_value1,selected_value2):
         category_data = bar_data[bar_data['category']==x]
         temp = []
         for y in nutrients:
-            temp.append(category_data[y].to_string(index=False))
-        temp = [float(i) for i in temp]
+            if(y.endswith('mg')):
+                temp.append(float(category_data[y].to_string(index=False))/1000)
+            else:
+                temp.append(float(category_data[y].to_string(index=False)))
+        
         category_nutrients.append(temp)
 
     figure = go.Figure(data=[
         go.Bar(name=category[0], x=nutrients, y=category_nutrients[0]),
-        go.Bar(name=category[1], x=nutrients, y=category_nutrients[1])
+        go.Bar(name=category[1], x=nutrients, y=category_nutrients[1]),
+        go.Bar(name='daily intake', x=nutrients, y=daily_dose)
     ])
 
     return figure
--- charts/bar_chart2.py
+++ charts/bar_chart2.py
@@ -10,6 +10,18 @@ from dataset.data import data
 class BarChart2:
     nutrients = ['carbohydrates_g', 'sugars_g', 'fat_g']
     category = ['side-dish', 'breakfast-and-brunch']
+    daily_nutrients_intake = {
+        'carbohydrates_g' : 260,
+        'sugars_g' : 90,
+        'fat_g' : 70,
+        'protein_g' : 50,
+        'dietary_fiber_g' : 25,
+        'sodium_mg' : 2300,
+        'calcium_mg' : 1000,
+        'iron_mg' : 14.8,
+        'magnesium_mg' : 400,
+        'potassium_mg' : 4700,
+    }
 
     bar_data = data.df.groupby("category").agg({'name':'count',nutrients[0]:'mean',nutrients[1]:'mean',nutrients[2]:'mean'}).reset_index()
     bar_data = bar_data[bar_data['name']>100]
@@ -24,9 +36,17 @@ class BarChart2:
         temp = [float(i) for i in temp]
         category_nutrients.append(temp)
 
+    daily_dose = []
+    for x in nutrients:
+        if(x.endswith('mg')):
+            daily_dose.append(daily_nutrients_intake[x]/1000)
+        else: 
+            daily_dose.append(daily_nutrients_intake[x])
+
     figure = go.Figure(data=[
         go.Bar(name=category[0], x=nutrients, y=category_nutrients[0]),
-        go.Bar(name=category[1], x=nutrients, y=category_nutrients[1])
+        go.Bar(name=category[1], x=nutrients, y=category_nutrients[1]),
+        go.Bar(name='daily intake', x=nutrients, y=daily_dose)
     ])
 
     # Change the bar mode
@@ -75,6 +95,35 @@ class BarChart2:
                 style = { 'width' : "95%" }
             ),
         ],className="row"),
+
+        html.Br(),
+
+        dcc.Dropdown(
+            id = 'bar_dropdown3',
+            options = [
+                { 'label' : 'Carbohydrate', 'value' : 'carbohydrates_g'},
+                { 'label' : 'Sugar', 'value' : 'sugars_g' },
+                { 'label' : 'Fat', 'value' : 'fat_g' },
+                { 'label' : 'Protein', 'value' : 'protein_g' },
+                { 'label' : 'Fiber', 'value' : 'dietary_fiber_g' },
+                { 'label' : 'Sodium', 'value' : 'sodium_mg' },
+                { 'label' : 'Calcium', 'value' : 'calcium_mg'},
+                { 'label' : 'Iron', 'value' : 'iron_mg'},
+                { 'label' : 'Magnesium', 'value' : 'magnesium_mg'},
+                { 'label' : 'Potassium', 'value' : 'potassium_mg'},
+            ],
+            optionHeight = 25,
+            value = nutrients,
+            disabled = False,
+            multi = True,
+            searchable = True,
+            search_value = '',
+            placeholder = 'Select...',
+            clearable = False,
+            style = { 'width' : "95%" }
+        ),
+
+        html.Br(),
         dcc.Graph(
             id='bar_graph2',
             figure=figure
