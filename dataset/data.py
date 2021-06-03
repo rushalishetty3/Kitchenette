cc0d83a3d4f8fdf510ba74352a0d4cb18623203a
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2021-06-03 23:37:38 +0530

    Add dict of data stats

--- dataset/data.py
+++ dataset/data.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
+
 class data:
     df = pd.read_csv("test-crawl8.csv")
+
     nutrients_df = pd.read_csv("nutrients.csv")
+
+    stats = {
+        'total_recipes': len(df),
+        'total_authors': df['author'].nunique(),
+        'total_reviews': df['review_count'].nunique(),
+        'total_ratings': df['rating_count'].nunique(),
+        'total_categories': df['category'].nunique(),
+    }
